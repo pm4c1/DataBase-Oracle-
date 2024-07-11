2f779b4355f3d33d0d13f539f54718a04c82b1d1
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-11 11:04:52 +0900

    Update funtion.sql

--- 관태쌤/24.07.11/select/funtion_select/funtion.sql
+++ 관태쌤/24.07.11/select/funtion_select/funtion.sql
@@ -6,3 +6,5 @@ select count(*), count(emp_grade) from emp_master;
 select count(*), count(emp_grade), sum(emp_pay) from emp_master;
 
 select count(*), count(emp_grade), sum(emp_pay), avg(emp_pay) from emp_master;
+
+select count(*), count(emp_grade), sum(emp_pay)/9, avg(emp_pay) from emp_master;
