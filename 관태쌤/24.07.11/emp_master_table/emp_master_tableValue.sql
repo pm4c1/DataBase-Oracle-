de102ff2644f04669435bd54bd571c042f8fa0ab
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-11 10:26:38 +0900

    Update emp_master_tableValue.sql

--- 관태쌤/24.07.11/emp_master_table/emp_master_tableValue.sql
+++ 관태쌤/24.07.11/emp_master_table/emp_master_tableValue.sql
@@ -10,7 +10,3 @@ insert into emp_master values('223', '정선희', '10', '30', '3400');
 insert into emp_master values('224', '김민석', '30', '30', '3900');
 insert into emp_master values('700', '우민주', null, null, '20');
 insert into emp_master values('701', '황남도', null, null, '10');
-
--- 테스트용 추가 값
-insert into emp_master values('900', '우민주', null, null, '20');
-insert into emp_master values('901', '황남도', null, null, '10');
