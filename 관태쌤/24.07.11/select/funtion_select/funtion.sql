be8f4e23bdb5d73de21b6e938c3f5039a3c2c02d
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-11 11:07:29 +0900

    Update funtion.sql

--- 관태쌤/24.07.11/select/funtion_select/funtion.sql
+++ 관태쌤/24.07.11/select/funtion_select/funtion.sql
@@ -1,6 +1,7 @@
 -- 모든 라인 수 조회
 select count(*) from emp_master;
 
+-- 모든 라인 수 + 직무코드 (null값 제외) 조회
 select count(*), count(emp_grade) from emp_master;
 
 select count(*), count(emp_grade), sum(emp_pay) from emp_master;
